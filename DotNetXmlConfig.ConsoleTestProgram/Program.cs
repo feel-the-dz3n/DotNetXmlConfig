ebbee98176b45ec32c2f0ba63bf5ff7b066dac32
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-05-25 17:29:15 +0300

    Add code

---
+++ DotNetXmlConfig.ConsoleTestProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetXmlConfig.ConsoleTestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers loaded:");
+
+            foreach (var number in ConfigMan.Numbers.Numbers)
+                Console.WriteLine(number.ToString());
+
+            while (true)
+            {
+                Console.WriteLine("Enter new number. Example: name|number");
+                Console.WriteLine("Enter 'q' to exit");
+
+                var line = Console.ReadLine();
+
+                if (line == "q") Environment.Exit(0);
+
+                string[] split = line.Split('|');
+
+                var n = new XmlNumber(split[0], split[1]);
+
+                ConfigMan.Numbers.Numbers.Add(n);
+
+                Console.WriteLine("Added:");
+                Console.WriteLine(n.ToString());
+            }
+        }
+    }
+}
---
+++ DotNetXmlConfig.ConsoleTestProgram/XmlNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetXmlConfig.ConsoleTestProgram
+{
+    [Serializable]
+    public class XmlNumbers : XmlConfig
+    {
+        public string ProgramName = "Program";
+        public string TableName = "Test Table";
+        public List<XmlNumber> Numbers = new List<XmlNumber>();
+    }
+
+    [Serializable]
+    public class XmlNumber
+    {
+        public string Name;
+        public string Number;
+
+        public XmlNumber() { }
+        public XmlNumber(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }
+
+        public override string ToString() => $"{Name} - {Number}";
+    }
+}
